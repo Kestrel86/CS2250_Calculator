acd7e91309dd8b49250d7fbece288c94dde0bde3
Kestrel86 <valdezandrew20@gmail.com>
2023-10-02 17:41:01 -0700

    new plans

--- script.js
+++ script.js
@@ -1,90 +1,46 @@
 const calculator = {
-  displayValue: "0",
-  firstOperand: null,
-  waitingForSecondOperand: false,
+  num1: null,
+  num2: null,
   operator: null,
 };
+
 const keys = document.querySelector(".keys");
 const display = document.querySelector(".screen");
 
-const updateDisplay = () => {
-  display.value = calculator.displayValue;
-};
-updateDisplay();
-
-keys.addEventListener("click", (event) => {
-  const { target } = event;
-  if (!target.matches("button")) {
-    return;
-  }
-
-  if (target.classList.contains("operator")) {
-    handleOperator(target.value);
-    updateDisplay();
-    return;
-  }
-
-  if (target.classList.contains("clear")) {
-    resetCalculator();
-    updateDisplay();
-    return;
-  }
-
-  inputDigit(target.value);
-  updateDisplay();
-});
-
-function inputDigit(digit) {
-  const { displayValue, waitingForSecondOperand } = calculator;
-
-  if (waitingForSecondOperand === true) {
-    calculator.displayValue = digit;
-    calculator.waitingForSecondOperand = false;
-  } else {
-    if (displayValue === "0") {
-      calculator.displayValue = digit;
-    } else {
-      calculator.displayValue = displayValue + digit;
-    }
-  }
+function handleOperator(operand) {
+  calculator.operator = operand;
+  display.value += "";
 }
 
-function handleOperator(nextOperator) {
-  const { firstOperand, displayValue, operator } = calculator;
-  const inputValue = parseFloat(displayValue);
-
-  if (operator && calculator.waitingForSecondOperand) {
-    calculator.operator = nextOperator;
-    return;
-  }
-  if (firstOperand == null && !isNaN(inputValue)) {
-    calculator.firstOperand = inputValue;
-  } else if (operator) {
-    const result = calculate(firstOperand, inputValue, operator);
-    calculator.displayValue = `${parseFloat(result)}`; //contemplated limit
-    calculator.firstOperand = result;
-  }
-
-  calculator.waitingForSecondOperand = true;
-  calculator.operator = nextOperator;
+function compute() {
+  calculate(calculator.operator);
 }
 
-function calculate(num1, num2, operator) {
+function clearAll() {
+  display.value = "";
+}
+
+function calculate(operator) {
   switch (operator) {
     case "+":
-      return num1 + num2;
+      var numArr = display.split("+");
+      var ans = Number(numArr[0]) + Number(numArr[1]);
+      break;
     case "-":
-      return num1 - num2;
+      val = num1 - num2;
+      break;
     case "*":
-      return num1 * num2;
+      val = num1 * num2;
+      break;
     case "/":
-      return num1 / num2;
+      val = num1 / num2;
+      break;
+    default:
+      val = "Error!!!";
   }
+  return val;
 }
 
-function resetCalculator() {
-  calculator.waitingForSecondOperand = false;
-  calculator.firstOperand = null;
-  calculator.displayValue = "0";
-  calculator.operator = null;
-}
+/*
+May Change format, found breakthrough with switch statement, revert to old javascript calculator
+*/
